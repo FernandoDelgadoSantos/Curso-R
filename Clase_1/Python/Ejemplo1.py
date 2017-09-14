6459373e200cbbf0176582fbb9ca5d5d20eea2f2
FernandoDelgadoSantos <kfdelgadosantos@gmail.com>
2017-09-14 17:59:11 -0500

    Ejemplos de clase

---
+++ Clase_1/Python/Ejemplo1.py
@@ -0,0 +1,10 @@
+def dia_1(dia):
+	if dia == "lune
+s":
+return ":("
+	if dia != "lunes":
+return ":D"
+
+print(dia_1("domingo"))
+print(dia_1("lunes"))
+
---
+++ Clase_1/Python/Ejemplo3.py
@@ -0,0 +1,24 @@
+x = 2
+
+if x > 3:
+        print("Este numero ")
+print("es mayor")
+print("qeu 3.")
+
+
+numeros = [1, 5, 2, 12, 24, 7, 18]
+
+doble = []
+for numero in numeros:
+    doble.append(2*numero)
+    
+numeros_pares = []
+for numero in numeros:
+    if numero % 2 == 0:
+        numero_pares.append(numero)
+
+animalitos = ["aardvark", "cat", "dog", "apossum"]
+animalitos_a = []
+for animalito in animalitos:
+    if animalito[0] in "aeiou":
+        animalitos_a.append(animalito.title())
